e28216ed7d9b9d9dd9a3899c139e2904be93a2ee
Michelle Maureny Muñoz Muñoz <michellemunoz@unicauca.edu.co>
2025-10-06 21:22:45 -0500

    Creacion del punto C de la clase 9, primera version

--- Microproyecto2/Clase9.X/LEDPARPADEO.c
+++ Microproyecto2/Clase9.X/LEDPARPADEO.c
@@ -5,7 +5,7 @@
  * Created on 3 de octubre de 2025, 07:56 AM
  */
 
-//Punto B
+//Punto C
 
 #include <xc.h>
 //#include <pic18f4550.h>
@@ -18,31 +18,59 @@
 
 #define _XTAL_FREQ 8000000        // Frecuencia de 8 MHz 
 
-void main(void){
-    OSCCON=0x72;                   //Configuración del oscilador de 8MHz
-    TRISBbits.TRISB0=0     //RB0 como salida digital
-    LATBbits.LATB0=0       //LED apagado al inicio 
-    unsigned int tiempo_total=20000; //20 segundos
-    unsigned int tiempo_ciclo=5*(500+500); 
-    
-    while (tiempo_total>0){
-        for (int i=0;i<5;i++){
-            LATBbits.LATB0=1  //LED ON
-            _delay_ms(500);
-            LATBbits.LATB0=0  //LED OFF
-            _delay_ms(500);
-        }
-        tiempo_total-=tiempo_ciclo; //5s
-    }
-    
-    for (int i=0;i<2;i++){
-        LATBbits.LATB0=1 //LED ON
-        _delay_ms(1000);
-        LATBbits.LATB0=0 //LED OFF
-        _delay_ms(1000);
-    }
-    
-    LATBbits.LATB0=0
-    while(1);
+void ADC_Init(void){
+    ADCON1=0x0E;           //AN0 analógico, resto digital
+    ADCON2=0xA9;
+    ADCON0=0x01;           //Habilita ADC, canal AN0
 }
-
+    
+unsigned int ADC_Read(unsigned char canal){
+    ADCON0 &=0xC5;     //Limpia canal
+    ADCON0 |=canal <<3; //Selecciona canal
+    __delay_ms(2); //Tiempo de adquisición 
+    GO_nDONE=1;   //Inicia conversación
+    while(GO_nDONE);  //Espera fin de conversación
+    return((ADRESH<<8)+ADRESL);  //Retorna resultado 
+}
+    
+void main(void){
+    OSCCON = 0x72;
+    TRISAbits.TRISA0=1;   //AN0 como entrada 
+    TRISBbits.TRISB0=0;   //LED rojo
+    TRISBbits.TRISB1=0;   //LED amarillo
+    TRISBbits.TRISB2=0;   //LED verde
+        
+    LATB=0x00;
+    ADC_Init();
+        
+    unsigned int lectura = 0; 
+    float porcentaje = 0; 
+        
+    while(1){
+        lectura=ADC_Read(0); //Lee canal AN0 (0-1023)
+        porcentaje = (lectura/1023.0)*100.0;
+            
+        if(porcentaje<25.0){
+            LATBbits.LATB0=1; //LED rojo ON
+            LATBbits.LATB1=0; 
+            LATBbits.LATB2=0; 
+        }
+            
+        else if(porcentaje>=25.0&&porcentaje<75.0){
+            LATBbits.LATB0=0; 
+            LATBbits.LATB1=1;   //LED amarillo ON
+            LATBbits.LATB2=0; 
+        }
+                 
+        else{
+            LATBbits.LATB0=0; 
+            LATBbits.LATB1=0;  
+            LATBbits.LATB2=1;  //LED verde ON
+        }
+                    
+        __delay_ms(200);  //Pequeña pausa 
+                 
+                
+    }
+            
+}
