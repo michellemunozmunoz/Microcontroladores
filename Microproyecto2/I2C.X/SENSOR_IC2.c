b5380e22150033e809b88bfa943d2102dcd9c503
Michelle Maureny Muñoz Muñoz <michellemunoz@unicauca.edu.co>
2025-10-13 16:16:56 -0500

    Ajuste en las librerias del codigo como en la logica del mismo, segundo avance

--- Microproyecto2/I2C.X/SENSOR_IC2.c
+++ Microproyecto2/I2C.X/SENSOR_IC2.c
@@ -7,66 +7,150 @@
 
 #include <xc.h>
 #include <stdint.h>
-#include <studio.h>
+#include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
-#pragma config FOSC = INTOSCIO_EC   //Oscilador interno, RA6/RA7 I/O
-#pragma config PLLDIV = 1            
-#pragma config CPUDIV = OSC1_PLL2         
-#pragma config USBDIV = 2
-#pragma config WDTEN = OFF   
-#pragma config LVP = OFF            
-#pragma config BOR = OFF            
+//Configuración básica
+#pragma config FOSC = INTOSCIO_EC   //Oscilador interno, RA6/RA7 E/S         
+#pragma config WDT = OFF   
+#pragma config LVP = OFF                       
 #pragma config MCLRE = ON
+#pragma config PBADEN = OFF 
 
+#define _XTAL_FREQ 8000000    //Frecuencia de reloj interno
 
-#define _XTAL_FREQ 8000000
-
-// RS RA0
-// EN RA1
-// D4 RD0
-// D5 RD1
-// D6 RD2
-// D7 RD3
-
-#define LCD_RS_LAT LATAbits.LATA0
-#define LCD_RS_TRIS TRISAbits.TRISA0 
-#define LCD_EN_LAT LATAbits.LATA1 
-#define LCD_EN_TRIS LATAbits.TRISA1
-
-
-#define LCD_D_TRIS TRISDbits.TRISD
-#define LCD_D_LAT LATD 
-
-//I2C por software en RB0 (SDA) y RB1 (SCL)
-
-#define SDA_TRIS TRISBbits.TRISB0 
-#define SDA_PORT PORTBbits.RB0  
-#define SDA_LAT LATBbits.LATB0
-
+//I2C SOFTWARE
+#define SDA_TRIS TRISBbits.TRISB0
 #define SCL_TRIS TRISBbits.TRISB1 
-#define SCL_PORT PORTBbits.RB1  
+#define SDA_LAT LATBbits.LATB0 
 #define SCL_LAT LATBbits.LATB1
+#define SDA_PORT PORTBbits.RB0
 
-//Ajuste de tiempos I2C 
+void I2C_Delay(void)
+{__delay_us(10);}
 
-static inline void 12c_delay_half(void){__delay_us(5); }  //5us
-static inline void 12c_delay(void){__delay_us(10); }  //10us
-
-void i2c_init(void) {
-    LATBbits.LATB0 = 1; 
-    LATBbits.LATB1 = 1;
-    SDA_TRIS = 1; //SDA release 
-    SCL_TRIS = 1; //SCL release    
+void I2C_Init(void){
+    SDA_TRIS = 1; SCL_TRIS = 1;
+    SDA_LAT = 1; SCL_LAT = 1; 
 }
 
-static inline void sda_release(void){LATBbits.LATB0 = 1; SDA_TRIS =1; }
-static inline void sda_low(void){LATBbits.LATB0 = 0; SDA_TRIS =0; }
-static inline uint8_t sda_read(void) {return PORTBbits.RB0; }
+void I2C_Start(void) { 
+    SDA_TRIS = 0; SDA_LAT = 1;
+    SCL_TRIS = 0; SCL_LAT = 1; 
+    I2C_Delay();
+    SDA_LAT= 0; I2C_Delay();
+    SCL_LAT = 0; 
+}
 
-static inline void scl_release(void){LATBbits.LATB1 = 1; SCL_TRIS =1; }
-static inline void scl_low(void){LATBbits.LATB1 = 0; SCL_TRIS =0; }
-static inline uint8_t sca_read(void) {return PORTBbits.RB1; } 
+void I2C_Stop(void) { 
+    SDA_TRIS = 0; SDA_LAT = 0; 
+    I2C_Delay();
+    SCL_LAT= 1; I2C_Delay();
+    SDA_LAT = 1; I2C_Delay();
+}
+
+void I2C_WriteBit(uint8_t bit) {
+    SDA_TRIS = 0; SDA_LAT = bit; 
+    I2C_Delay(); SCL_LAT= 1; 
+    I2C_Delay(); SCL_LAT = 0;  
+}
+
+uint8_t I2C_ReadBit(void) {
+    uint8_t bit;
+    SDA_TRIS = 1;
+    I2C_Delay(); SCL_LAT= 1; 
+    I2C_Delay(); bit = SDA_PORT;
+    SCL_LAT=0;
+    return bit; 
+}
+
+uint8_t I2C_Write(uint8_t data) {
+    for (int i = 0; i<8; i++) {
+        I2C_WriteBit((data & 0x80)!=0);
+        data <<= 1;
+        }
+        return !I2C_ReadBit();    //ACK = 0    
+        }
+
+uint8_t I2C_Read(uint8_t ack) {
+    uint8_t data = 0;
+    SDA_TRIS = 1;
+    for (int i = 0; i<8; i++) {
+        data <<= 1;
+        if(I2C_ReadBit())data|=1;   
+        }
+        I2C_WriteBit(!ack);
+        return data;
+    }
+    
+    //LCD 16x2 MODO 4 BITS
+    
+#define LCD_RS LATAbits.LATA0
+#define LCD_EN LATAbits.LATA1
+#define LCD_D4 LATAbits.LATA2
+#define LCD_D5 LATAbits.LATA3
+#define LCD_D6 LATAbits.LATA4
+#define LCD_D7 LATAbits.LATA5
+#define LCD_PORT_DIR TRISA
+    
+void LCD_Pulse(void){
+    LCD_EN = 1;__delay_us(40);
+    LCD_EN = 0;__delay_us(40);
+}
+
+void LCD_Cmd(uint8_t cmd){
+    LCD_RS = 0;
+    LCD_D4 = (cmd>>4)&1;
+    LCD_D5 = (cmd>>5)&1;
+    LCD_D6 = (cmd>>6)&1;
+    LCD_D7 = (cmd>>7)&1;
+    LCD_Pulse();
+    
+    LCD_D4 = cmd&1;
+    LCD_D5 = (cmd>>1)&1;
+    LCD_D6 = (cmd>>2)&1;
+    LCD_D7 = (cmd>>3)&1;
+    LCD_Pulse();
+    
+    __delay_ms(2);  
+}
+
+void LCD_Init(void){
+    LCD_PORT_DIR=0x00;
+    __delay_ms(20);
+    LCD_Cmd(0x02);
+    LCD_Cmd(0x28);
+    LCD_Cmd(0x0C);
+    LCD_Cmd(0x06);
+    LCD_Cmd(0x01);
+     __delay_ms(2);  
+}
+
+void LCD_SetCursor(uint8_t row, uint8_t col){
+    uint8_t address = (row == 1)? 0x80 : 0xC0;
+    address +=(col-1);
+    LCD_Cmd(address);   
+}
+
+void LCD_WriteChar(char c){
+    LCD_RS = 1;
+    LCD_D4 = (c>>4)&1;
+    LCD_D5 = (c>>5)&1;
+    LCD_D6 = (c>>6)&1;
+    LCD_D7 = (c>>7)&1;
+    LCD_Pulse();
+    LCD_D4 = c & 1;
+    LCD_D5 = (c>>1)&1;
+    LCD_D6 = (c>>2)&1;
+    LCD_D7 = (c>>3)&1;
+    LCD_Pulse();
+    __delay_us(100);         
+}
+
+void LCD_WriteString(const char*str){
+    while(*str)LCD_WriteChar(*str++);
+}
 
 
 
